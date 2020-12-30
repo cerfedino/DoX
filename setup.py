49f84e607404e1739437725c269229e98634a966
Albert Cerfeda <cerfeda.albert@gmail.com>
2020-12-30 13:21:50 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='usi-discord-bot',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='cerfe',
+    author_email='cerfeda.albert@gmail.com',
+    description=''
+)
