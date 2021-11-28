7fcb8ef95be07edefd59a075b3f20d14acaeb6e4
Albert Cerfeda <cerfeda.albert@gmail.com>
2021-11-28 21:38:17 +0100

    Added login and register form listeners

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,15 @@
 function init_login() {
+    const form = document.querySelector(".login-form form")
 
+    form.addEventListener("submit",function(e){
+        e.preventDefault()
+        // TODO: Send login fields to POST /auth
+        // fetch(form.action,
+        //     {method:"POST"
+        //     })
+
+
+        // TODO: Remove after demo
+        window.location = "/docs"
+    })
 }
--- public/js/register.js
+++ public/js/register.js
@@ -1,3 +1,15 @@
 function init_register() {
+    const form = document.querySelector(".login-form form")
 
+    form.addEventListener("submit",function(e){
+        e.preventDefault()
+        // TODO: Send register fields to POST /auth/register
+        // fetch(form.action,
+        //     {method:"POST"
+        //     })
+
+
+        // TODO: Remove after demo
+        window.location = "/login"
+    })
 }
