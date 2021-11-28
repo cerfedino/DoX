f6d41acf02d3cfeafae3608ee18482974636367a
Albert Cerfeda <cerfeda.albert@gmail.com>
2021-11-28 21:37:38 +0100

    Commented and refined routes

--- routes/root.js
+++ routes/root.js
@@ -14,13 +14,27 @@ module.exports = router;
 
 // PARAMETERS
 const public = path.resolve(__dirname, "../public/")
+////////////////
 
+// ###############
+// GET REQUESTS
+// ###############
+
+/*
+    GET /
+    If authentication is succesfull, redirects to the documents view.
+        Otherwise, redirects to the GET /login
+ */
 router.get('/', function (req, res) {
     //TODO: Implement auth
 
     res.redirect("/login")
 })
 
+/*
+    GET /login
+    Renders the login form.
+ */
 router.get('/login', function (req, res) {
     if (req.accepts("text/html")) {
         res.render('../views/login.ejs', {});
@@ -29,7 +43,10 @@ router.get('/login', function (req, res) {
     }
 })
 
-
+/*
+    GET /register
+    Renders the register form.
+ */
 router.get('/register', function (req, res) {
     if (req.accepts("text/html")) {
         res.render("../views/register.ejs")
@@ -38,17 +55,51 @@ router.get('/register', function (req, res) {
     }
 })
 
+/*
+    GET /docs/:id
+    Renders the document edit view for the specified document.
+    IF the user only has read access to it, renders accordingly.
+        IF the user has NO access to it, denies access to it.
+ */
+router.get('/docs/:id?', function (req, res) {
+    if(req.params.id) {
+        // TODO: Check if user is allowed to view/edit document
+        res.render('../views/edit.ejs')
+    } else {
+        res.render('../views/documents.ejs')
+    }
 
-router.get('/doc/:id', function (req, res) {
-    // TODO: Check if user is allowed to view/edit document
-    res.render('../views/edit.ejs')
 })
 
-// Serves files inside the /public folder.
+/*
+    GET /*
+    Serves the files in the /public folder, if tehy exist.
+ */
 router.get("/*", (req,res)=>{
     const path = `${public}${req.path}`
     if (fs.pathExistsSync(path)) {
         res.status(200).sendFile(path)
     } else
         res.status(404).end()
+})
+
+
+// ###############
+// POST REQUESTS
+// ###############
+
+/*
+    POST /auth
+    Authenticates a user with credentials
+ */
+router.post("/auth",(req,res)=>{
+
+})
+
+/*
+    POST /auth/register
+    Registers a new user
+ */
+router.post("/auth/register",(req,res)=>{
+
 })
