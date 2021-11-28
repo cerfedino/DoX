cc83bd57983398fe81436e410178635dbaab684b
Albert Cerfeda <cerfeda.albert@gmail.com>
2021-11-28 16:42:03 +0100

    Improved models/index.js

--- models/index.js
+++ models/index.js
@@ -2,27 +2,26 @@
  * Web Atelier 2021  Final Project
  *
  * MongoDB Database
- * 
+ *
  * The database contains 2 collections:
- * 
+ *
  *       USERS
  *       { 
  *           {ObjectId} _id, 
  *           {string}   username, 
- *           {string}   password_hash, 
+ *           {string}   password,
  *           {string}   email,
  *           {boolean}  email_verification_status
- *       } 
- * 
+ *       }
+ *
  *
  *       DOCS
- *       { 
+ *       {
+ *           {String} name,
  *           {ObjectId} _id, 
  *           {URL} path, 
- *           perms: { 
- *                   read: [], 
- *                   edit: [] 
- *           }, 
+ *           perm_read: [],
+ *     	     perm_edit: [],
  *           {ObjectId} owner, 
  *       
  *           {URL} read_link, 
@@ -30,32 +29,35 @@
  *       
  *           {Date} edit_date, 
  *           {Date} created_date 
- *       } 
+ *       }
  *
  */
 
 
 const mongodb = require('mongodb');
-const ObjectId = mongodb.ObjectId;
 const MongoClient = mongodb.MongoClient;
 
-const mongodb_uri = 'mongodb://localhost:27017';
-const db_name = 'SA3-Final-Project';
+/**
+ * Fetches the database and the collections.
+ */
 
-const users_collection_name = 'users';
-const docs_collection_name = 'docs';
+///// PARAMETERS
+const mongodb_uri = "mongodb://localhost:27017"
+const db_name     = "DoX_db";
+const collections = ["users","docs"];
+////////////////////
 
-const model = {};
 
+const model = {}
 MongoClient
-    .connect(mongodb_uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .connect(mongodb_uri, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(client => {
-        console.log("Connected to " + mongodb_uri);
         model.db = client.db(db_name);
-        model.users = model.db.collection(users_collection_name);
-        model.docs = model.db.collection(docs_collection_name);
-        
+        collections.forEach(k => {
+            model[k] = model.db.collection(k)
+        })
+        console.log("[+] Fetched MongoDB database and collections")
+        // require('./sync').check(model.music, "public/music").then(console.log);
     })
-    .catch(err => console.error(err));
 
-exports.model = model;
+exports.model = model
