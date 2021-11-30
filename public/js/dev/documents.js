259bc713ec9b00d0dfa121dbc9968114d4b3fd08
Denis Karev <dkarev@protonmail.com>
2021-11-30 18:18:14 +0100

    basic keymap for the editor

--- public/js/dev/documents.js
+++ public/js/dev/documents.js
@@ -1,10 +1,23 @@
 const {schema} = require('prosemirror-schema-basic');
 const {EditorState} = require('prosemirror-state');
-const {EditorView} = require('prosemirror-view')
+const {EditorView} = require('prosemirror-view');
+const {undo, redo, history} = require('prosemirror-history');
+const {keymap} = require('prosemirror-keymap');
+const {baseKeymap} = require('prosemirror-commands');
 
-let state = EditorState.create({schema});
+let state = EditorState.create({
+    schema,
+    plugins: [
+        history(),
+        keymap({'Mod-z': undo, 'Mod-y': redo}),
+        keymap(baseKeymap)
+    ]
+});
 let view = new EditorView(document.getElementById("editor"), {state});
 
+view.focus();
+
+// Dark mode
 document.querySelector("#dark-mode-toggle").addEventListener('change',
     function () {
         if (this.checked)
