03edf175e10d5b2618d69eb5316d6318e5ff9867
Denis Karev <dkarev@protonmail.com>
2021-11-30 19:47:39 +0100

    functional menu (bold and italic for now)

--- public/js/dev/documents.js
+++ public/js/dev/documents.js
@@ -3,19 +3,7 @@ const {Plugin, EditorState} = require('prosemirror-state');
 const {EditorView} = require('prosemirror-view');
 const {undo, redo, history} = require('prosemirror-history');
 const {keymap} = require('prosemirror-keymap');
-const {baseKeymap} = require('prosemirror-commands');
-
-let state = EditorState.create({
-    schema,
-    plugins: [
-        history(),
-        keymap({'Mod-z': undo, 'Mod-y': redo}),
-        keymap(baseKeymap)
-    ]
-});
-let view = new EditorView(document.getElementById("editor"), {state});
-
-view.focus();
+const {baseKeymap, toggleMark} = require('prosemirror-commands');
 
 /**
  * Menu component
@@ -23,7 +11,7 @@ view.focus();
 class MenuView {
     constructor(items, editorView) {
         /**
-         * Array of objects with command and dom properties.
+         * Array of objects with name, type, command and dom roperties.
          * @type {Array}
          */
         this.items = items;
@@ -32,7 +20,7 @@ class MenuView {
         this.dom = document.getElementById('actions');
         this.update();
 
-        this.dom.addEventListener("click", e => {
+        this.dom.addEventListener("mousedown", e => {
             e.preventDefault();
             editorView.focus();
             items.forEach(({command, dom}) => {
@@ -43,11 +31,97 @@ class MenuView {
     }
 
     update() {
-        this.items.forEach(({command, dom}) => {
-            let active = command(this.editorView.state, null, this.editorView)
-            if (active) dom.classList.add('active');
-            else dom.classList.remove('active');
-        })
+        let activeMarks = getActiveMarkCodes(this.editorView);
+        this.items.forEach(item => {
+            if (item.type === 'mark') {
+                if (activeMarks.includes(item.name)) item.dom.classList.add('active');
+                else item.dom.classList.remove('active');
+            } else {
+                let active = item.command(this.editorView.state, null, this.editorView)
+                if (active) item.dom.classList.add('active');
+                else item.dom.classList.remove('active');
+            }
+        });
+    }
+}
+
+initEditor();
+
+function initEditor() {
+    let menu = menuPlugin([
+        {
+            name: 'strong',
+            type: 'mark',
+            command: toggleMark(schema.marks.strong),
+            dom: document.getElementById('action-bold')
+        },
+        {
+            name: 'em',
+            type: 'mark',
+            command: toggleMark(schema.marks.em),
+            dom: document.getElementById('action-italic')
+        }
+    ])
+
+    let state = EditorState.create({
+        schema,
+        plugins: [
+            history(),
+            keymap({'Mod-z': undo, 'Mod-y': redo}),
+            keymap(baseKeymap),
+            menu
+        ]
+    });
+    let editorView = new EditorView(document.getElementById("editor"), {state});
+
+    editorView.focus();
+}
+
+/**
+ * Plugin view for the editor
+ * @param items Array of objects with command and dom properties
+ * @returns {MenuView|Plugin}
+ */
+function menuPlugin(items) {
+    return new Plugin({
+        view(editorView) {
+            return new MenuView(items, editorView);
+        }
+    })
+}
+
+/**
+ * Gets active marks on the current selection
+ * @author https://github.com/PierBover/
+ */
+function getActiveMarkCodes (view) {
+    const isEmpty = view.state.selection.empty;
+    const state = view.state;
+
+    if (isEmpty) {
+        const $from = view.state.selection.$from;
+        const storedMarks = state.storedMarks;
+
+        // Return either the stored marks, or the marks at the cursor position.
+        // Stored marks are the marks that are going to be applied to the next input
+        // if you dispatched a mark toggle with an empty cursor.
+        if (storedMarks) {
+            return storedMarks.map((mark) => mark.type.name);
+        } else {
+            return $from.marks().map((mark) => mark.type.name);
+        }
+    } else {
+        const $head = view.state.selection.$head;
+        const $anchor = view.state.selection.$anchor;
+
+        // We're using a Set to not get duplicate values
+        const activeMarks = new Set();
+
+        // Here we're getting the marks at the head and anchor of the selection
+        $head.marks().forEach((mark) => activeMarks.add(mark.type.name));
+        $anchor.marks().forEach((mark) => activeMarks.add(mark.type.name));
+
+        return Array.from(activeMarks);
     }
 }
 
