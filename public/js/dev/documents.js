2f13ece99e460a4761c1168f1a2d54eab71589de
Denis Karev <dkarev@protonmail.com>
2021-11-30 18:34:53 +0100

    Menu view component

--- public/js/dev/documents.js
+++ public/js/dev/documents.js
@@ -1,5 +1,5 @@
 const {schema} = require('prosemirror-schema-basic');
-const {EditorState} = require('prosemirror-state');
+const {Plugin, EditorState} = require('prosemirror-state');
 const {EditorView} = require('prosemirror-view');
 const {undo, redo, history} = require('prosemirror-history');
 const {keymap} = require('prosemirror-keymap');
@@ -16,6 +16,40 @@ let state = EditorState.create({
 let view = new EditorView(document.getElementById("editor"), {state});
 
 view.focus();
+
+/**
+ * Menu component
+ */
+class MenuView {
+    constructor(items, editorView) {
+        /**
+         * Array of objects with command and dom properties.
+         * @type {Array}
+         */
+        this.items = items;
+        this.editorView = editorView;
+
+        this.dom = document.getElementById('actions');
+        this.update();
+
+        this.dom.addEventListener("click", e => {
+            e.preventDefault();
+            editorView.focus();
+            items.forEach(({command, dom}) => {
+                if (dom.contains(e.target))
+                    command(editorView.state, editorView.dispatch, editorView);
+            })
+        })
+    }
+
+    update() {
+        this.items.forEach(({command, dom}) => {
+            let active = command(this.editorView.state, null, this.editorView)
+            if (active) dom.classList.add('active');
+            else dom.classList.remove('active');
+        })
+    }
+}
 
 // Dark mode
 document.querySelector("#dark-mode-toggle").addEventListener('change',
