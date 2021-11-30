0f9a1ba0aef255f8848b491b49f1fe0e9ea6ee03
Denis Karev <dkarev@protonmail.com>
2021-11-30 02:30:01 +0100

    editor mvp

---
+++ public/js/dev/documents.js
@@ -0,0 +1,14 @@
+const {schema} = require('prosemirror-schema-basic');
+const {EditorState} = require('prosemirror-state');
+const {EditorView} = require('prosemirror-view')
+
+let state = EditorState.create({schema});
+let view = new EditorView(document.body, {state});
+
+document.querySelector("#dark-mode-toggle").addEventListener('change',
+    function () {
+        if (this.checked)
+            document.body.classList.add("dark-theme")
+        else
+            document.body.classList.remove('dark-theme')
+    });
