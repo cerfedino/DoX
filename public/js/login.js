d0f09fd66aba0ed9d1a69e19183f46e4ab203546
Alessandro-Gobbetti <ale.gobbetti.personal@gmail.com>
2021-12-01 10:53:27 +0100

    fixed login form connection to passport

--- public/js/login.js
+++ public/js/login.js
@@ -1,17 +1,4 @@
 function init_login() {
-    const form = document.querySelector(".login-form form")
-
-    form.addEventListener("submit",function(e){
-        e.preventDefault()
-        // TODO: Send login fields to POST /auth
-        // fetch(form.action,
-        //     {method:"POST"
-        //     })
-
-
-        // TODO: Remove after demo
-        window.location = "/docs"
-    })
 
     document.querySelector("#dark-mode-toggle").addEventListener('change',
         function() {
