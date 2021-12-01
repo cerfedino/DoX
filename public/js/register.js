1f2b234a1f55a032d9eccb6cd7cc6a34d385e177
Alessandro-Gobbetti <ale.gobbetti.personal@gmail.com>
2021-12-01 10:56:28 +0100

    fixed fetch form submission

--- public/js/register.js
+++ public/js/register.js
@@ -1,17 +1,4 @@
 function init_register() {
-    const form = document.querySelector(".login-form form")
-
-    // form.addEventListener("submit",function(e){
-    //     e.preventDefault()
-    //     // TODO: Send register fields to POST /auth/register
-    //     // fetch(form.action,
-    //     //     {method:"POST"
-    //     //     })
-
-
-    //     // TODO: Remove after demo
-    //     // window.location = "/login"
-    // })
 
     document.querySelector("#dark-mode-toggle").addEventListener('change',
         function() {
