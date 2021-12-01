4d81edb820ba9109e5743c731dd29e90ab743d5c
Alessandro-Gobbetti <ale.gobbetti.personal@gmail.com>
2021-12-01 10:53:46 +0100

    fixed register form connection to passport

--- public/js/register.js
+++ public/js/register.js
@@ -1,17 +1,17 @@
 function init_register() {
     const form = document.querySelector(".login-form form")
 
-    form.addEventListener("submit",function(e){
-        e.preventDefault()
-        // TODO: Send register fields to POST /auth/register
-        // fetch(form.action,
-        //     {method:"POST"
-        //     })
+    // form.addEventListener("submit",function(e){
+    //     e.preventDefault()
+    //     // TODO: Send register fields to POST /auth/register
+    //     // fetch(form.action,
+    //     //     {method:"POST"
+    //     //     })
 
 
-        // TODO: Remove after demo
-        window.location = "/login"
-    })
+    //     // TODO: Remove after demo
+    //     // window.location = "/login"
+    // })
 
     document.querySelector("#dark-mode-toggle").addEventListener('change',
         function() {
