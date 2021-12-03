cff88cae6257adde7a930f6048035fd977e81b8c
Albert Cerfeda <cerfeda.albert@gmail.com>
2021-12-03 18:36:39 +0100

    Added centralized settings inside of module settings.js

--- .email-credentials.js
+++
@@ -1,9 +0,0 @@
-
-// This file contains the email credentials that we need to send email
-// please do not share it
-
-module.exports =
-{
-    user: 'noreply.mailserver.dox@gmail.com', // our mail
-    pass: 'srcaszbhkohfmlnn'  // app password (it does not allow login from browser)
-}
--- modules/mailing.js
+++ modules/mailing.js
@@ -6,7 +6,7 @@
 
 const nodemailer = require('nodemailer');
 
-const email_credentials = require('../.email-credentials.js')
+const email_credentials = require('../config.js').mailing;
 
 
 
