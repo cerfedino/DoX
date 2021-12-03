d93581e406431ccc1a230c5c739aa52ef3b1fd51
Alessandro-Gobbetti <ale.gobbetti.personal@gmail.com>
2021-12-03 12:08:03 +0100

    added email credentials, to be removed

---
+++ .email-credentials.js
@@ -0,0 +1,9 @@
+
+// This file contains the email credentials that we need to send email
+// please do not share it
+
+module.exports =
+{
+    user: 'noreply.mailserver.dox@gmail.com', // our mail
+    pass: 'srcaszbhkohfmlnn'  // app password (it does not allow login from browser)
+}
