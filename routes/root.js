4bcc3f2c7009c3ebb34fa4ff6e5b6018cc6b4008
Alessandro-Gobbetti <ale.gobbetti.personal@gmail.com>
2021-12-19 16:02:13 +0000

    fixed profile pic, removed unused rout for profile pics

--- routes/root.js
+++ routes/root.js
@@ -179,29 +179,6 @@ router.get('/users/:id', async function (req, res){
     }
 })
 
-// Gets user profile picture based on ID
-
-router.get("/pic/users/:id", async function (req, res){
-    let id;
-    try {
-        id = ObjectId(req.params.id);
-    } catch(err) {
-        res.status(404).end();
-    }
-    let user = await dbops.user_find({_id : id});
-    if (user) { 
-        if (req.accepts("image/png")) {
-            res.sendFile(user.profile_pic);
-        } else {
-            console.log("err")
-        } 
-    } else {
-        res.status(404).end();
-    }
-    
-
-})
-
 // ###############
 // PUT REQUESTS
 // ###############
