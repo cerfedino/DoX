d48143f183bed3660ae8ece19e3b2c3cc5c67e2d
Albert Cerfeda <cerfeda.albert@gmail.com>
2023-07-17 11:00:57 +0200

    Docker-compose, .env config, nodemon and other stuff

--- server.js
+++ server.js
@@ -1,21 +1,8 @@
 const config = require('./config/config.js')
 const app = require("./app.js")
-// const fs = require('fs-extra')
 
 const websocket = require("./modules/socket.js")
 
-
-if(config.webserver.https_enabled) {
-    // const key = fs.readFileSync("localhost-key.pem", "utf-8");
-    // const cert = fs.readFileSync("localhost.pem", "utf-8");
-    // https.createServer({ key, cert }, app).listen(config.webserver.port);
-
-    // TODO: Add HTTPS encryption if remote.
-    const server = require('http').createServer(app);
-    websocket.init(server)
-    server.listen(config.webserver.port);
-} else {
-    const server = require('http').createServer(app);
-    websocket.init(server)
-    server.listen(config.webserver.port);
-}
+const server = require('http').createServer(app);
+websocket.init(server)
+server.listen(config.webserver.port);
